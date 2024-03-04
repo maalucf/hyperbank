2c721c81aa4e37bc4e2491efbd94e0da1c5314e4
maalucf <mari.luiza.fig@gmail.com>
2024-03-04 18:01:46 -0300

    added react build + server alterations

--- hyperbank-back/src/services/server/server.js
+++ hyperbank-back/src/services/server/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const path = require('path')
 
 //Middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.static(path.join(__dirname, '../../../../hyperbank-front/build')));
 
 //Routes
 app.use('/', require('../controller/loginController'));
@@ -15,6 +17,10 @@ app.use('/', require('../controller/incomeController'));
 app.use('/', require('../controller/methodController'));
 app.use('/', require('../controller/categoryController'));
 app.use('/', require('../controller/transactionController'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../../../hyperbank-front/build/index.html'));
+});
 
 app.listen(5000, () => console.log('Server running on port 5000'));
 
